220a945d2e4f81a626704999b1fcb1130ec54aed
CreditHaat <rahulgupta.credithaat@gmail.com>
2024-06-30 21:37:50 +0530

    embedded list image chnages

--- src/components/EmbeddedJourneyList/CHEmbeddedList.js
+++ src/components/EmbeddedJourneyList/CHEmbeddedList.js
@@ -76,13 +76,17 @@ const LendersList = ({ json1, mobileNumber, isVisible, setIsVisible, OTPGenerate
         <div key={index} className={lender.product} >
           <div  className="card-container">
               <div className="card-content">
-                <img alt="logo" src={lender.logo} className="logo" />
-                <div className="text-content">
+              <div className="" style={{width:'50%',float:'left',margin:'auto'}}>
+              <div className="text-content" style={{width:'50%',padding:'10px',margin:'auto',marginLeft:'0px'}}>
+                <img alt="logo" src={lender.logo} className="logo"  style={{display:'flex',justifyContent:'center',width:'100%',height:'auto',maxWidth:'120px',minWidth:'90px'}}/>
+              </div>
+                </div>
+                <div className="text-content" style={{width:'50%',float:'right'}}>
                   <Typography variant="h5" component="div" className="title" style={{ fontFamily: 'Times New Roman, Times, serif' }}>
                     {lender.product}
                   </Typography>
                   <Typography variant="body2" color="textSecondary" className="data" style={{ fontFamily: 'Times New Roman, Times, serif' }}>
-                    Quick Personal Loans, 100% digital collateral-free
+                  {lender.description}
                   </Typography>
                 </div>
               </div>
